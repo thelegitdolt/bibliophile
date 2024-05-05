a1079826d952ef444d64f9118b715a4f7c116f5c
thelegitdolt <asphaltpotatofy@gmail.com>
2024-05-04 17:40:05 -0700

    fuck it, no allurement bs, get overwritten bitch

--- src/main/java/com/dolthhaven/bibliophile/core/mixin/EnchantmentTableBlockMixin.java
+++ src/main/java/com/dolthhaven/bibliophile/core/mixin/EnchantmentTableBlockMixin.java
@@ -1,24 +1,24 @@
 package com.dolthhaven.bibliophile.core.mixin;
 
 import com.dolthhaven.bibliophile.data.server.tags.BibliophileItemTags;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.EnchantmentTableBlock;
 import net.minecraft.world.level.block.entity.ChiseledBookShelfBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.extensions.IForgeBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(IForgeBlock.class)
-public class IForgeBlockMixin {
-    @Inject(method = "getEnchantPowerBonus", at = @At("RETURN"), cancellable = true, remap = false)
-    private void Bibliophile$ChiseledBookshelfEnchantingPower(BlockState state, LevelReader level, BlockPos pos, CallbackInfoReturnable<Float> cir) {
+@Mixin(EnchantmentTableBlock.class)
+public class EnchantmentTableBlockMixin {
+    @WrapOperation(method = "isValidBookShelf", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getEnchantPowerBonus(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;)F"))
+    private static float Bibliophile$ChiseledBookshelfIsAlsoCorrectLol(BlockState self, LevelReader level, BlockPos pos, Operation<Float> original) {
         if (level.getBlockEntity(pos) instanceof ChiseledBookShelfBlockEntity bookcase) {
-            // Power from Chainsaw Man? Holyshit?
             float power = 0f;
             for (int i = 0; i < bookcase.getContainerSize(); i++) {
                 ItemStack stack = bookcase.getItem(i);
@@ -27,13 +27,16 @@ public class IForgeBlockMixin {
                 }
 
                 if (stack.is(BibliophileItemTags.MAGICAL_BOOKS)) {
-                    power += 0.5f;
+                    power += 2f;
                     continue;
                 }
 
-                power += 1 / 3f;
+                power += 1f;
             }
-            cir.setReturnValue(power);
+            return power;
+        }
+        else {
+            return original.call(self, level, pos);
         }
     }
 }
